0ac1e77f61f8399783fe9c8ad8d7235da73d7e9e
Vivian Dagbue <vdagbue@gmail.com>
2023-08-10 09:27:43 -0700

    fix: reloaded json string

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,7 +11,6 @@ class FileStorage:
 
     __file_path = "file.json"
     __objects = {}
-    __cls = ""
 
     def all(self):
         """Returns the dictionary __objects"""
@@ -20,33 +19,38 @@ class FileStorage:
     def new(self, obj):
         """Sets the obj and the key in __objects"""
         if obj:
-            self.__cls = obj.__class__.__name__
             self.__objects[(
-                obj.__class__.__name__ + "." + obj.id)] = obj.__dict__
+                obj.__class__.__name__ + "." + obj.id)] = obj
 
     def save(self):
         """Serializes __objects to the JSON file"""
-        copy = {outer_key: dict(
-            inner_dict) for outer_key, inner_dict in self.__objects.items()}
+        copy = {}
+        for key, val in self.__objects.items():
+            obj_dict = val.to_dict()
+            copy[key] = dict(obj_dict)
         for value in copy.values():
             if 'updated_at' in value and type(value['updated_at']) != str:
                 value['updated_at'] = value[
                     'updated_at'].strftime("%Y-%m-%dT%H:%M:%S.%f")
-            value['__class__'] = self.__cls
             if 'created_at' in value and type(value['created_at']) != str:
                 value['created_at'] = value[
                     'created_at'].strftime("%Y-%m-%dT%H:%M:%S.%f")
         with open(self.__file_path, mode='w', encoding="utf-8") as f:
             json.dump(copy, f)
 
+    def storage_import(self, result):
+        """Imports the storge module"""
+        from models import storage
+        from models.base_model import BaseModel
+        objects = {}
+        for obj_id, obj_data in result.items():
+            obj = BaseModel(**obj_data)
+            objects[obj_id] = obj
+        return objects
+
     def reload(self):
         """Deserializes the JSON file to __objects"""
         if os.path.isfile(self.__file_path):
             with open(self.__file_path, mode='r', encoding="utf-8") as f:
-                self.__objects = json.load(f)
-        for key, value in self.__objects.items():
-            value['updated_at'] = datetime.strptime(
-                value['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            value['created_at'] = datetime.strptime(
-                value['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            del (value['__class__'])
+                result = json.load(f)
+            self.__objects = self.storage_import(result)
